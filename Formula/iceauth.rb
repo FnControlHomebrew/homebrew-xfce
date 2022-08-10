06b40850dac7a116384385b1cd56de401a80e1c2
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-10 18:20:44 +0000

    iceauth: add 1.0.9 bottle.

--- Formula/iceauth.rb
+++ Formula/iceauth.rb
@@ -5,6 +5,11 @@ class Iceauth < Formula
   sha256 "2cb9dfcb545683af77fb1029bea3fc52dcc8a0666f7b8b2d7373b6ed4c408c05"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/brewOS/homebrew-xfce/releases/download/iceauth-1.0.9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "26d4f5e54d594a52c915088f1a013e6358de385435e8856314a28c7fc1a6808d"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libice"
   depends_on "xorgproto"
