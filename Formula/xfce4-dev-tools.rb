10d0e1d836defdae6649026c7afef4f5872d01de
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-10 17:59:22 +0000

    xfce4-dev-tools: add 4.16.0 bottle.

--- Formula/xfce4-dev-tools.rb
+++ Formula/xfce4-dev-tools.rb
@@ -5,6 +5,11 @@ class Xfce4DevTools < Formula
   sha256 "f50b3070e66f3ebdf331744dd1ec5e1af5de333965d491e15ce05545e8eb4f04"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/brewOS/homebrew-xfce/releases/download/xfce4-dev-tools-4.16.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5f32afebb8d6ce0d3a721bc7d5daccd5a2dbc88cd09a5bb29c663e723c843c6a"
+  end
+
   depends_on "automake" => :build
   depends_on "gtk-doc" => :build
   depends_on "intltool" => :build
