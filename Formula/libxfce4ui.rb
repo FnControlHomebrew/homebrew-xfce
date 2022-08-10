40b9e4d2a0046ed3eb6ed2423f0cf04b8d133bad
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-10 15:15:10 -0700

    libxfce4ui 4.16.1 (new formula)
    Closes #5.
    
    Signed-off-by: fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>

---
+++ Formula/libxfce4ui.rb
@@ -0,0 +1,34 @@
+class Libxfce4ui < Formula
+  desc "Commonly used Xfce widgets among Xfce applications"
+  homepage "https://xfce.org/"
+  url "https://archive.xfce.org/src/xfce/libxfce4ui/4.16/libxfce4ui-4.16.1.tar.bz2"
+  sha256 "d96946ae5af6bf078dda415419e0021909f763ee0020b42f3e26f603e51585f6"
+  license "LGPL-2.0-or-later"
+
+  depends_on "brewos/xfce/xfce4-dev-tools" => :build
+  depends_on "gettext" => :build
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "brewos/xfce/libxfce4util"
+  depends_on "brewos/xfce/xfconf"
+  depends_on "glade"
+  depends_on "glib"
+  depends_on "gtk+3"
+  depends_on "libgtop"
+  depends_on "libsm"
+  depends_on "startup-notification"
+
+  uses_from_macos "perl" => :build
+  uses_from_macos "libxml2"
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5" unless OS.mac?
+
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate prefix, :exist? # stub
+  end
+end
