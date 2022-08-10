738d8ed3cd92fa6ba67cf7d912d07cb10e84578d
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-10 11:41:44 -0700

    xfconf 4.16.0 (new formula)
    Closes #4.
    
    Signed-off-by: fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>

---
+++ Formula/xfconf.rb
@@ -0,0 +1,27 @@
+class Xfconf < Formula
+  desc "Xfce hierarchical (tree-like) configuration system"
+  homepage "https://xfce.org/"
+  url "https://archive.xfce.org/src/xfce/xfconf/4.16/xfconf-4.16.0.tar.bz2"
+  sha256 "652a119007c67d9ba6c0bc7a740c923d33f32d03dc76dfc7ba682584e72a5425"
+  license all_of: ["GPL-2.0-only", "LGPL-2.0-only"]
+
+  depends_on "gettext" => :build
+  depends_on "glib-utils" => :build
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libxfce4util"
+
+  uses_from_macos "perl" => :build
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5" unless OS.mac?
+
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate prefix, :exist? # stub
+  end
+end
